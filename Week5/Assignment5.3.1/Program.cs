e459b962aef917dc2a0d0639abb600c889c0c030
Ryan <kinisonryan@outlook.com>
2025-08-06 13:30:34 -0600

    8/6/2025 Assignments

---
+++ Week5/Assignment5.3.1/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Assignment5._3._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] flowerBed = { 1, 0, 0, 0, 0, 0, 1 };
+            int newFlowersGoal = 2;
+           Console.WriteLine( CheckArray(flowerBed, newFlowersGoal));
+        }
+        static bool CheckArray(int[] flowerBed, int newFlowersGoal)
+        {
+            int newFlowersSum = 0;
+            int counter = 0;
+            for(int i = 0; i < flowerBed.Length; i++)
+            {
+                if (flowerBed[i] == 0)
+                {
+                    counter++;
+                }
+                else
+                {
+                    counter = 0;
+                }
+                if(counter == 3)
+                {
+                    newFlowersSum++;
+                    counter = 1;
+                }
+                if(newFlowersSum == newFlowersGoal)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ Week5/Assignment5.3.2/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Assignment5._3._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int stairsNum = 6;
+            Console.WriteLine(climbVariations(stairsNum));
+        }
+        static int climbVariations(int stairs)
+        {
+            int variations1 = 0;
+            int variations2 = 1;
+            for(int i = 0; i < stairs; i++)
+            {
+                variations2 = variations1 + variations2;
+                variations1 = variations2 - variations1;
+            }
+            return variations2;
+        }
+    }
+}
