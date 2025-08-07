d11f2798f5f0a0377ee40aafcc32d5618485621f
Ryan <kinisonryan@outlook.com>
2025-08-07 14:40:01 -0600

    5.4 Assignments

---
+++ Week5/Assignment5.4.1/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Assignment5._4._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputData = 1234;
+            printDigit(inputData);
+        }
+        static void printDigit(int data)
+        {
+
+            if (data > 9)
+            {
+                printDigit(data / 10);
+            }
+            Console.Write(data % 10 + " ");
+        }
+    }
+}
---
+++ Week5/Assignment5.4.2/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Assignment5._4._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] matrix = { { 1, 2, 3, }, { 4, 5, 6 }, { 7, 8, 9 } };
+            int sum = 0;
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int i2 = 0; i2 < matrix.GetLength(1); i2++)
+                {
+                    Console.Write(matrix[i, i2]);
+                }
+                Console.WriteLine();
+                sum += matrix[i, i];
+            }
+            Console.WriteLine("\n");
+            Console.WriteLine(sum);
+        }
+    }
+}
