179ad2179d119a52cb6bd61de83795748f25cfcf
Ryan <kinisonryan@outlook.com>
2025-08-07 16:34:28 -0600

    Week 5 Challenge Labs

---
+++ Week5/ChallengeLab5.1/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace ChallengeLab5._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] integers = { 2, 2, 1};
+            Dictionary<int, int> count = new Dictionary<int, int>();
+            for (int i = 0; i < integers.Length; i++)
+            {
+                if (count.ContainsKey(integers[i]))
+                {
+                    count[integers[i]]++;
+                }
+                else
+                {
+                   count.Add(integers[i], 1);
+                }
+            }
+            foreach (int i in count.Keys)
+            {
+                if (count[i] == 1)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
---
+++ Week5/ChallengeLab5.2/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace ChallengeLab5._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = { 9, 6, 4, 2, 3, 5, 7, 0, 1 };
+            input = Sort(input);
+            Console.WriteLine(Search(input));
+        }
+        static int[] Sort(int[] input)
+        {
+            int temp = 0;
+            bool change = true;
+            while (change)
+            {
+                change = false;
+                for (int i = 0; i < input.Length; i++)
+                {
+                    if (i < input.Length - 1)
+                    {
+                        if (input[i] > input[i + 1])
+                        {
+                            temp = input[i];
+                            input[i] = input[i + 1];
+                            input[i + 1] = temp;
+                            change = true;
+                        }
+                    }
+                }
+            }
+            return input;
+        }
+        static int Search(int[] input)
+        {
+            for (int i = 0; i < input.Length;i++)
+            {
+                if (input[i] != i)
+                {
+                    return (i);
+                }
+            }
+            return input.Length;
+        }
+    }
+}
