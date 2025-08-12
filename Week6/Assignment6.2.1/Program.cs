2f9698c7e00396242d7ab77edd063f0687f023fe
Ryan <kinisonryan@outlook.com>
2025-08-12 13:04:59 -0600

    6.2 Assignments

---
+++ Week6/Assignment6.2.1/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace Assignment6._2._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack stack = new Stack();
+            stack.push(10);
+            stack.push(20);
+            stack.push(30);
+            stack.Display();
+            stack.pop();
+            stack.pop();
+            stack.push(50);
+            stack.Display();
+        }
+    }
+    internal class Stack
+    {
+        private int[] ints = new int[5];
+        private int position = -1;
+
+        public void push(int data)
+        {
+            if (position == ints.Length)
+            {
+                throw new Exception("Stack is full");
+            }
+            else
+            {
+                ints[++position] = data;
+            }
+        }
+        public int pop()
+        {
+            int data = 0;
+            if (position < 0)
+            {
+                throw new Exception("Stack is empty");
+            }
+            else
+            {
+                data = ints[position--];
+            }
+                return data;
+        }
+        public int peek()
+        {
+            return ints[position];
+        }
+        public void Display()
+        {
+            for (int i = position; i > -1; i--)
+            {
+                Console.WriteLine(ints[i]);
+            }
+        }
+    }
+}
---
+++ Week6/Assignment6.2.2/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace Assignment6._2._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] data = { 2, 3, 4, 5, 6 };
+            int[] left = new int[data.Length];
+            int[] right = new int[data.Length];
+            for (int i = 0; i < data.Length; i++)
+            {
+                if (i ==0)
+                {
+                    left[i] = data[i];
+                }
+                else
+                {
+                    left[i] = left[i-1] * data[i];
+                }
+            }
+            for (int i = data.Length - 1;i >= 0; i--)
+            {
+                if (i == data.Length - 1)
+                {
+                    right[i] = data[i];
+                }
+                else
+                {
+                    right[i] = right[i + 1] * data[i];
+                }
+            }
+            for (int i = 0; i < data.Length; i++)
+            {
+                if (i == 0)
+                {
+                    data[i] = right[i + 1];
+                }
+                else if (i == data.Length - 1)
+                {
+                    data[i] = left[i - 1];
+                }
+                else
+                {
+                    data[i] = left[i - 1] * right[i + 1];
+                }
+                Console.WriteLine(data[i]);
+            }
+
+        }
+    }
+}
