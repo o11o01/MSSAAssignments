15ff14cdb7ded73e06dad179f6238de995dcf92e
Ryan <kinisonryan@outlook.com>
2025-08-20 16:16:03 -0600

    Assignment 7.3.1

---
+++ Week7/Assignment7.3.1/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Assignment7._3._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Tree tree1 = new Tree();
+            int[] ints = { 20, 4, 46, 5, 9, 12, 9, 8, 7, };
+            for(int i = 0; i < ints.Length; i++)
+            {
+                tree1.add(ints[i]);
+            }
+            Tree tree2 = new Tree();
+            tree2.Root = tree1.search(9);
+        }
+    }
+}
---
+++ Week7/Assignment7.3.1/Tree.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment7._3._1
+{
+    public class Tree
+    {
+        public Node Root
+        { get; set; }
+        public Node Temp
+        { get; set; }
+        public void add(int newValue)
+        {
+            if(Root == null)
+            {
+                Root = new Node(newValue);
+            }
+            else
+            {
+                Root.add(newValue);
+            }
+        }
+        public Node search(int searchTerm)
+        {
+            Temp = Root;
+            while(Temp.left != null || Temp.right != null)
+            {
+                if(searchTerm == Temp.value)
+                {
+                    return Temp;
+                }
+                else if(searchTerm < Temp.value)
+                {
+                    Temp = Temp.left;
+                }
+                else
+                {
+                    Temp = Temp.right;
+                }
+            }
+            return null;
+        }
+    }
+    public class Node
+    {
+        public Node left
+        { get; set; }
+        public Node right
+        { get; set; }
+        public int value
+        { get; set; }
+        public void add(int newValue)
+        {
+            if (newValue < value)
+            {
+                if (left == null)
+                {
+                    left = new Node(newValue);
+                }
+                else
+                {
+                    left.add(newValue);
+                }
+            }
+            else
+            {
+                if (right == null)
+                {
+                    right = new Node(newValue);
+                }
+                else
+                {
+                    right.add(newValue);
+                }
+            }
+        }
+        public Node(int passValue)
+        {
+            value = passValue;
+        }
+    }
+}
